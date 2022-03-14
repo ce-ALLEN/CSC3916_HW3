14b5e58ca129d0bda6d8e180063288724f4ecbb9
Christopher Allen <93797239+ce-ALLEN@users.noreply.github.com>
2022-03-14 12:48:45 -0600

    updated Movies.js

--- Movies.js
+++ Movies.js
@@ -3,6 +3,15 @@ var Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
 //movies schema
 var MovieSchema = new Schema({
     title: {type: String, required: true},
