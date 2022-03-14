ea61fcb3aa2d8faf5b4d0eee99bbb52ccc20d93b
Christopher Allen <93797239+ce-ALLEN@users.noreply.github.com>
2022-03-14 12:43:33 -0600

    updated Movies.js

--- Movies.js
+++ Movies.js
@@ -6,10 +6,10 @@ mongoose.Promise = global.Promise;
 //movies schema
 var MovieSchema = new Schema({
     title: {type: String, required: true},
-    yearReleased: {type: Date, required: true},
+    yearReleased: {type: String, required: true},
     genre: {type: String, required: true},
-    actors: [{actor1Name: String, character1Name: String}, {actor2Name: String, character2Name: String},
-        {actor3Name: String, character3Name: String}]
+    actors: [{actorName: String, characterName: String}, {actorName: String, characterName: String},
+        {actorName: String, characterName: String}]
 });
 
 
